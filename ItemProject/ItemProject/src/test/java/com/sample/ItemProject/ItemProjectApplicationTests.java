e9447a6ad0895ded1e4cca712a2df496acf9244b
devopsasp <85219013+devopsasp@users.noreply.github.com>
2022-04-11 14:22:15 +0530

    Add files via upload

---
+++ ItemProject/ItemProject/src/test/java/com/sample/ItemProject/ItemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.ItemProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
